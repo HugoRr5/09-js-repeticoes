d29c3c872280ea8d990368c0fa84fe7959fa8087
HugoRr5 <132697347+HugoRr5@users.noreply.github.com>
2023-07-08 01:13:43 +0000

    testes

--- script.js
+++ script.js
@@ -6,19 +6,23 @@ function contar(){
 
     if(ini.value.length ==0|| fim.value.length ==0 || passo.value.length ==0){
         window.alert('[ERRO] faltam dados')
-        res.innerHTML="impossivel contar"
+        res.innerHTML='impossivel contar'
     }else{
         res.innerHTML = 'Contando:'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if(p<=0){
+            window.alert('passo invalido')
+            p = 1
+        }
         if (i<f){
         for (let c = i; c <= f ; c += p ){
-            res.innerHTML += '${c}'
+            res.innerHTML += '  $(c)  '
 
         }
         }else {
         for (let c = i; c >= f ; c += p ){
-             res.innerHTML += '${c}'
+             res.innerHTML += '  $(c)  \u{1F449}'
     
     }}}}
