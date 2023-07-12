0966c5b595159b11f98deb6526599e96f255b96b
HugoRr5 <132697347+HugoRr5@users.noreply.github.com>
2023-07-11 21:08:15 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ function contar(){
                 res.innerHTML += ` ${c}  \u{1F449} `
     
     }}}}
+    function tabuada(){
+        let num = document.getElementById('txtn')
+        let tab = document.getElementById('seltab')  
+        if (num.value.length==0){
+            window.alert ('porfavor tente denovo e digite um numero')
+        }else {
+                let n = Number(num.value)
+                
+    
+        }
+    
+    
+        }
